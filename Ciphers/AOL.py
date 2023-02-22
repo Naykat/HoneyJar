27f43ced01780cb877ac54718acd5725d351ee25
Naykat <65075625+Naykat@users.noreply.github.com>
2023-02-22 15:03:55 +0300

    Update AOL.py

--- Ciphers/AOL.py
+++ Ciphers/AOL.py
@@ -5,11 +5,12 @@ from Utilities.utils import RaiseTypeError, getType, bSet, findPos, createString
 
 class AOL():
     def __init__(self, alphabet: str = '''ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()}{[]"№%:,.;_+-=/?\|±§<>'0123456789  ⠀''') -> None:
-        if  not(type(alphabet) == str):
+        if not(type(alphabet) == str):
             currentType = getType(alphabet)
             RaiseTypeError('string', 'str', currentType)
         alphabet = alphabet.replace(' ','⠀').replace('0','௦').replace('1','௧').replace('2','௨').replace('3','௩').replace('4','௪').replace('5','௫').replace('6','௬').replace('7','௭').replace('8','௮').replace('9','௯')
         
+        #Constant
         self.__alphabet = bSet(alphabet)
         
     def encode(self, string: str) -> str:
@@ -37,21 +38,21 @@ class AOL():
         string = ' '+string
         string = string[::-1]
         lap = {}
-        sep_letters_array = createArrayWithDividedEncodedLetters(string)
+        symbols = createArrayWithDividedEncodedLetters(string)
         
-        for counter in range(len(sep_letters_array)):
-            alphabet+=sep_letters_array[counter][0]
-        for i in range(len(sep_letters_array)):
-            current_string = sep_letters_array[i]
+        for letter in range(len(symbols)):
+            alphabet+=symbols[letter][0]
+        for letter in range(len(symbols)):
+            current_string = symbols[letter]
             current_letter = getLetter(current_string)
             current_string = current_string[1:]
-            current_array = []
+            array_with_places = []
             while current_string!='':
                 ministring = createStringToDecode(current_string)
                 letter_pos = findPos(ministring, self.__alphabet)
-                current_array.append(letter_pos)
+                array_with_places.append(letter_pos)
                 current_string = current_string.replace(ministring,'',1)
-            lap[current_letter] = current_array
+            lap[current_letter] = array_with_places
         result = ''
         for letters in range(getStringLength(lap, alphabet)):
             result+=getCharPos(lap, letters, alphabet)
@@ -59,5 +60,3 @@ class AOL():
         result = result.replace(' ','⠀').replace('௦','0').replace('௧','1').replace('௨','2').replace('௩','3').replace('௪','4').replace('௫','5').replace('௬','6').replace('௭','7').replace('௮','8').replace('௯','9')
         
         return result
-
-
